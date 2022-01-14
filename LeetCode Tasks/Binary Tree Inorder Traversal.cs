e4250e0de88a080d18972877c8547f2bea5a2770
FatCatMilkweed <mr.milkweed@mail.ru>
2022-01-14 17:07:04 +0500

    Binary Tree Inorder Traversal solution

---
+++ LeetCode Tasks/Binary Tree Inorder Traversal.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode_Tasks.CommonDataStructures;
+
+namespace LeetCode_Tasks
+{
+    public class BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode? root)
+        {
+            var stack = new Stack<TreeNode>();
+            var result = new List<int>();
+            if (root == null)
+                return result;
+
+            var current = root;
+            
+            while (current != null || stack.Any())
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.Left;
+                }
+
+                current = stack.Pop();
+                result.Add(current.Val);
+                current = current.Right;
+            }
+
+            return result;
+        }
+    }
+}
